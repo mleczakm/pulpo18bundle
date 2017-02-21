01f4db3c210075fba2647fe81da1d8f3420ac59e
Michał Mleczko <kontakt@michalmleczko.waw.pl>
2017-02-21 22:16:39 +0100

    Add symfony command

---
+++ src/Command/Pulpo18Command.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mleczakm
+ * Date: 21.02.17
+ * Time: 21:47
+ */
+
+namespace mleczakm\Pulpo18Bundle\Command;
+
+use mleczakm\PlatformBased\Executor;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DocCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('pulpo')
+            ->setDescription('Generates schema of your entities')
+            ->addOption(
+                'import-project',
+                'import-project',
+                InputOption::VALUE_REQUIRED,
+                'Project directory'
+            )
+            ->addOption(
+                'orm',
+                'orm',
+                InputOption::VALUE_REQUIRED,
+                'ORM version (Doctrine, Doctrine2 or Propel), default Doctrine2',
+                'Doctrine2'
+            )
+            ->addOption(
+                'export-image',
+                'export-image',
+                InputOption::VALUE_REQUIRED,
+                'Where should the image should be generated',
+                __DIR__
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $executor = new Executor();
+
+        $executor->execute(
+            __DIR__ . DIRECTORY_SEPARATOR . '..' .DIRECTORY_SEPARATOR . 'pulpo' . DIRECTORY_SEPARATOR . 'Pulpo',
+            sprintf(
+                '-import-project %s -orm %s -export-image %s',
+                $input->getOption('import-project'),
+                $input->getOption('orm'),
+                $input->getOption('export-image')
+            )
+        );
+
+        $output->writeln('OK');
+    }
+}
---
+++ src/Pulpo18Bundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace mleczakm\Pulpo18Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Created by PhpStorm.
+ * User: mleczakm
+ * Date: 21.02.17
+ * Time: 21:39
+ */
+class Pulpo18Bundle extends Bundle
+{
+
+}
